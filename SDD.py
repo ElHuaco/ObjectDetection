0b616d50cbf5a0c118c2578fe1abd8459a50f514
ElHuaco <alexhuac@gmail.com>
2023-01-13 17:50:13 +0100

    SSD arch

--- SDD.py
+++ SDD.py
@@ -15,7 +15,8 @@ class ScaleMap(nn.Module):
         self.norm1 = nn.BatchNorm2d(channels[1])
         self.conv2 = nn.Conv2d(channels[1], channels[2], kernel_size=3, stride=1)
         self.norm2 = nn.BatchNorm2d(channels[2])
-        self.feature_map = nn.Conv2d(channels[2], box_num * (4 + class_num), kernel_size=3, stride=1)
+        self.offset = nn.Conv2d(channels[2], box_num * 4, kernel_size=3, stride=1)
+        self.confid = nn.Conv2d(channels[2], box_num * class_num, kernel_size=3, stride=1)
 
     def _init_weights(self, module):
         pass
@@ -23,8 +24,9 @@ class ScaleMap(nn.Module):
     def forward(self, x):
         x = F.relu(self.norm1(self.conv1(x)))
         x = F.relu(self.norm2(self.conv2(x)))
-        features = self.feature_map(x)
-        return x, features
+        offset = self.offset(x)
+        confid = self.confid(x)
+        return x, offset, confid
 
 
 class SSD(nn.Module):
@@ -36,32 +38,35 @@ class SSD(nn.Module):
         #    self.base_network = Inception()
         else:
             raise ValueError('SSD base network')
-        # 6 escalas
         vgg_out = 42
-        self.scale_one = nn.Conv2d(vgg_out, 4 * (4 + class_num), kernel_size=3, stride=1)
+        self.scale1_offs = nn.Conv2d(vgg_out, 4 * 4, kernel_size=3, stride=1)
+        self.scale1_conf = nn.Conv2d(vgg_out, 4 * class_num, kernel_size=3, stride=1)
         self.conv1 = nn.Conv2d(vgg_out, 1024, kernel_size=3, stride=1)
         self.norm1 = nn.BatchNorm2d(1024)
         self.conv2 = nn.Conv2d(1024, 1024, kernel_size=1, stride=1)
         self.norm2 = nn.BatchNorm2d(1024)
-        self.scale_two = nn.Conv2d(1024, 6 * (4 + class_num), kernel_size=3, stride=1)
-        self.scale_three = ScaleMap((1024, 256, 512), 6, class_num)
-        self.scale_four = ScaleMap((512, 128, 256), 6, class_num)
-        self.scale_five = ScaleMap((256, 128, 256), 4, class_num)
-        self.scale_six = ScaleMap((256, 128, 256), 4, class_num)
+        self.scale2_offs = nn.Conv2d(1024, 6 * 4, kernel_size=3, stride=1)
+        self.scale2_conf = nn.Conv2d(1024, 6 * class_num, kernel_size=3, stride=1)
+        self.scale3 = ScaleMap((1024, 256, 512), 6, class_num)
+        self.scale4 = ScaleMap((512, 128, 256), 6, class_num)
+        self.scale5 = ScaleMap((256, 128, 256), 4, class_num)
+        self.scale6 = ScaleMap((256, 128, 256), 4, class_num)
 
     def _init_weights(self, module):
         pass
 
     def forward(self, x):
         x = self.base_network(x)
-        scale_one_out = self.scale_one(x)
+        scale1_offs, scale1_conf = self.scale1_offs(x), self.scale1_conf(x)
         x = F.relu(self.norm1(self.conv1(x)))
         x = F.relu(self.norm2(self.conv2(x)))
-        scale_two_out = self.scale_two(x)
-        x, scale_three_out = self.scale_three(x)
-        x, scale_four_out = self.scale_four(x)
-        x, scale_five_out = self.scale_five(x)
-        x, scale_six_out = self.scale_six(x)
+        scale2_offs, scale2_conf = self.scale2_offs(x), self.scale2_conf(x)
+        x, scale3_offs, scale3_conf = self.scale3(x)
+        x, scale4_offs, scale4_conf = self.scale4(x)
+        x, scale5_offs, scale5_conf = self.scale5(x)
+        _, scale6_offs, scale6_conf = self.scale6(x)
         # Asumimos que la prediccion es una lista de tensores (c + 4) x DBs x H x W para cada escala
         #   Para cada escala: (n_cats + 4) * n_default_boxes * H_scale * W_scale
-        pass
+        return (scale1_offs, scale2_offs, scale3_offs, scale4_offs, scale5_offs, scale6_offs),\
+               (scale1_conf, scale2_conf, scale3_conf, scale4_conf, scale5_conf, scale6_conf)
+        # cómo se calcula la pérdida desde el hard mining si esta desestima algunos outputs?
