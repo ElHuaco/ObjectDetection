0dc84970d7184fe5305aeec3e84df1f7ae3b392d
ElHuaco <alexhuac@gmail.com>
2023-01-15 17:06:47 +0100

    Added correct prediction shape. Refactored loader.

--- SSD.py
+++ SSD.py
@@ -11,12 +11,14 @@ class ScaleMap(nn.Module):
         super(ScaleMap, self).__init__()
         if len(channels) != 3:
             raise ValueError('ConvBlock must have exactly 3 channel values')
+        self.box_num = box_num
+        self.class_num = class_num
         self.conv1 = nn.Conv2d(channels[0], channels[1], kernel_size=1, stride=1)
         self.norm1 = nn.BatchNorm2d(channels[1])
         self.conv2 = nn.Conv2d(channels[1], channels[2], kernel_size=3, stride=second_stride)
         self.norm2 = nn.BatchNorm2d(channels[2])
-        self.offset = nn.Conv2d(channels[2], box_num * 4, kernel_size=3, stride=1)
-        self.confid = nn.Conv2d(channels[2], box_num * class_num, kernel_size=3, stride=1)
+        self.offset = nn.Conv2d(channels[2], self.box_num * 4, kernel_size=3, stride=1)
+        self.confid = nn.Conv2d(channels[2], self.box_num * self.class_num, kernel_size=3, stride=1)
 
     def _init_weights(self, module):
         pass
@@ -24,8 +26,11 @@ class ScaleMap(nn.Module):
     def forward(self, x):
         x = F.relu(self.norm1(self.conv1(x)))
         x = F.relu(self.norm2(self.conv2(x)))
-        offset = self.offset(x)
-        confid = self.confid(x)
+        _, _, h, w = x.size
+        # Prediction offset shape: Batch x (H * W * Priors) x 4
+        offset = torch.reshape(self.offset(x), (-1, h * w * self.box_num, 4))
+        # Prediction offset shape: Batch x (H * W * Priors) x Classes
+        confid = torch.reshape(self.confid(x), (-1, h * w * self.box_num, self.class_num))
         return x, offset, confid
 
 
@@ -38,35 +43,38 @@ class SSD(nn.Module):
         #    self.base_network = Inception()
         else:
             raise ValueError('SSD base network')
+        self.class_num = class_num
         vgg_out = 42
         self.scale1_offs = nn.Conv2d(vgg_out, 4 * 4, kernel_size=3, stride=1)
-        self.scale1_conf = nn.Conv2d(vgg_out, 4 * class_num, kernel_size=3, stride=1)
+        self.scale1_conf = nn.Conv2d(vgg_out, 4 * self.class_num, kernel_size=3, stride=1)
         self.conv1 = nn.Conv2d(vgg_out, 1024, kernel_size=3, stride=1)
         self.norm1 = nn.BatchNorm2d(1024)
         self.conv2 = nn.Conv2d(1024, 1024, kernel_size=1, stride=1)
         self.norm2 = nn.BatchNorm2d(1024)
         self.scale2_offs = nn.Conv2d(1024, 6 * 4, kernel_size=3, stride=1)
-        self.scale2_conf = nn.Conv2d(1024, 6 * class_num, kernel_size=3, stride=1)
-        self.scale3 = ScaleMap((1024, 256, 512), 6, class_num, second_stride=2)
-        self.scale4 = ScaleMap((512, 128, 256), 6, class_num, second_stride=2)
-        self.scale5 = ScaleMap((256, 128, 256), 4, class_num, second_stride=1)
-        self.scale6 = ScaleMap((256, 128, 256), 4, class_num, second_stride=1)
+        self.scale2_conf = nn.Conv2d(1024, 6 * self.class_num, kernel_size=3, stride=1)
+        self.scale3 = ScaleMap((1024, 256, 512), 6, self.class_num, second_stride=2)
+        self.scale4 = ScaleMap((512, 128, 256), 6, self.class_num, second_stride=2)
+        self.scale5 = ScaleMap((256, 128, 256), 4, self.class_num, second_stride=1)
+        self.scale6 = ScaleMap((256, 128, 256), 4, self.class_num, second_stride=1)
 
     def _init_weights(self, module):
         pass
 
     def forward(self, x):
         x = self.base_network(x)
-        scale1_offs, scale1_conf = self.scale1_offs(x), self.scale1_conf(x)
+        _, _, h, w = x.size
+        scale1_offs = torch.reshape(self.scale1_offs(x), (-1, h * w * 4, 4))
+        scale1_conf = torch.reshape(self.scale1_conf(x), (-1, h * w * 4, self.class_num))
         x = F.relu(self.norm1(self.conv1(x)))
         x = F.relu(self.norm2(self.conv2(x)))
-        scale2_offs, scale2_conf = self.scale2_offs(x), self.scale2_conf(x)
+        _, _, h, w = x.size
+        scale2_offs = torch.reshape(self.scale2_offs(x), (-1, h * w * 6, 4))
+        scale2_conf = torch.reshape(self.scale2_conf(x), (-1, h * w * 6, self.class_num))
         x, scale3_offs, scale3_conf = self.scale3(x)
         x, scale4_offs, scale4_conf = self.scale4(x)
         x, scale5_offs, scale5_conf = self.scale5(x)
         _, scale6_offs, scale6_conf = self.scale6(x)
-        # Asumimos que la prediccion es una lista de tensores (c + 4) x DBs x H x W para cada escala
-        #   Para cada escala: (n_cats + 4) * n_default_boxes * H_scale * W_scale
-        return (scale1_offs, scale2_offs, scale3_offs, scale4_offs, scale5_offs, scale6_offs),\
-               (scale1_conf, scale2_conf, scale3_conf, scale4_conf, scale5_conf, scale6_conf)
-        # cómo se calcula la pérdida desde el hard mining si esta desestima algunos outputs?
+        offs = torch.cat((scale1_offs, scale2_offs, scale3_offs, scale4_offs, scale5_offs, scale6_offs), dim=1)
+        conf = torch.cat((scale1_conf, scale2_conf, scale3_conf, scale4_conf, scale5_conf, scale6_conf), dim=1)
+        return offs, conf
