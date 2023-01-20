deedf58f9e4aa23e106089eba93a4e5244a40441
ElHuaco <alexhuac@gmail.com>
2023-01-20 21:52:42 +0100

    Conflict solved.

--- SSDLoss.py
+++ SSDLoss.py
@@ -31,7 +31,6 @@ class SSDLoss(nn.Module):
         for b in range(pred_boxes.size(0)):
             print('    computing matches...')
             matches = matching(pred_boxes[b], gt_boxes[b])  # (N, M) tensor of booleans relating predictions and GT boxes
-<<<<<<< HEAD
             # Get loss for every box
             box_matches = matches.sum(dim=1, dtype=torch.bool)
             total_matches = box_matches.sum()
@@ -57,56 +56,10 @@ class SSDLoss(nn.Module):
             negative_losses = conf_softmax[torch.logical_not(box_matches)] # da tensor de softmax(confidences) de pred_boxes sin match
             negative_losses = torch.max(negative_losses, dim=1) # coge los maximos solo
             sorted_negatives, indices = torch.sort(negative_losses.values, descending=True)
-=======
-            print(f'{pred_confidences[b] =}')
-            print('    Doing batch', b)
-            # Get loss for every box
-            box_matches = matches.sum(dim=1, dtype=torch.bool)
-            total_matches = box_matches.sum()
-            conf_softmax1 = F.softmax(pred_confidences[b], dim=1)
-            conf_softmax2 = conf_softmax1.clone()
-            
-            # Get the indices of the matches
-            indx_matches = torch.argwhere(matches)
-            
-            print('    loc loss...')
-            # Location loss
-            coords_matches_gt = torch.index_select(gt_boxes[b], 0, indx_matches[:, 1])
-            coords_matches_pred = torch.index_select(pred_boxes[b], 0, indx_matches[:, 0])
-            loc_loss = nn.SmoothL1Loss()(coords_matches_pred, coords_matches_gt)
-            print(f'{loc_loss = }')
-            print('    conf loss...')
-            # Positive confidences loss
-            indx_matches[:,1] = torch.argwhere(torch.index_select(gt_labels[b], 0, indx_matches[:, 1]))[:,1]
-            confs_matches_pred = conf_softmax1[indx_matches[:,0], indx_matches[:,1]]
-            print(f'{confs_matches_pred = }')
-            matches_loss = torch.sum(torch.log(confs_matches_pred))
-            print(f'{matches_loss = }')
-            print('    Starting hard mining...')
-            # Hard negative mining
-            negative_losses = conf_softmax2[torch.logical_not(box_matches)] # da tensor de softmax(confidences) de pred_boxes sin match
-            negative_losses = torch.max(negative_losses, dim=1) # coge los maximos solo
-            sorted_negatives, indices = torch.sort(negative_losses.values, descending=True)
-            print('    Entering try-except block...')
->>>>>>> 4ba6e98e8be1986faecb823b886838176e987329
             try:
                 kept_neg_losses = sorted_negatives[:(self.hmr * total_matches)]
             except IndexError:
                 kept_neg_losses = sorted_negatives
-            
-<<<<<<< HEAD
             nomatch_loss = torch.sum(torch.log(1 + kept_neg_losses))
             loss += (-matches_loss + nomatch_loss + loc_loss) / total_matches
-=======
-            print('    computing nomatch_los..')
-            nomatch_loss = (torch.log(1 + kept_neg_losses)).sum()
-            print(f'{nomatch_loss = }')
-            print('    adding to loss...')
-            print(((-matches_loss + nomatch_loss + loc_loss) / total_matches).shape)
-            print((-matches_loss + nomatch_loss + loc_loss) / total_matches)
-            return (-matches_loss + nomatch_loss + loc_loss) / total_matches
-            loss += (-matches_loss + nomatch_loss + loc_loss) / total_matches
-            print('finished batch.')
-        print('finished loss.')
->>>>>>> 4ba6e98e8be1986faecb823b886838176e987329
         return loss
